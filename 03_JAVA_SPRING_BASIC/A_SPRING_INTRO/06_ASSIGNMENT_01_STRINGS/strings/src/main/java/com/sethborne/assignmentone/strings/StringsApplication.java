3215b36fb8633a2d9532fb6bbc8f2fb10d65cc62
Seth <sethborne@gmail.com>
2017-07-17 12:20:55 -0700

    03 Java Spring Basic - 06 Assignment 01 - Strings

---
+++ 03_JAVA_SPRING_BASIC/A_SPRING_INTRO/06_ASSIGNMENT_01_STRINGS/strings/src/main/java/com/sethborne/assignmentone/strings/StringsApplication.java
@@ -0,0 +1,29 @@
+package com.sethborne.assignmentone.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	
+	public String hello() {
+		return "Hello Client! How are you Doing?";
+	}
+	
+	@RequestMapping("/random")
+	
+	public String random() {
+		return "Spring Boot is great! So Easy to just Respond with Strings.";
+	}
+}
