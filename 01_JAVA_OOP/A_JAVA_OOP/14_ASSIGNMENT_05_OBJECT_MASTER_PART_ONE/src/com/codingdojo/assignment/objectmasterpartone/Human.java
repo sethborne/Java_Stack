b798f29a5578e65dfedc7b33fe5c18e264041df6
Seth <sethborne@gmail.com>
2017-07-29 07:58:09 -0700

    JAVA OOP - 14 - Assignment 05 - Object Master Part One - First Commit

--- 01_JAVA_OOP/A_JAVA_OOP/14_ASSIGNMENT_05_OBJECT_MASTER_PART_ONE/src/com/codingdojo/assignment/objectmasterpartone/Human.java
+++ 01_JAVA_OOP/A_JAVA_OOP/14_ASSIGNMENT_05_OBJECT_MASTER_PART_ONE/src/com/codingdojo/assignment/objectmasterpartone/Human.java
@@ -1,6 +1,48 @@
 package com.codingdojo.assignment.objectmasterpartone;
 
 public class Human {
-	private int var = 0;
+	private int strength = 3;
+	private int intellegence = 3;
+	private int stealth = 3;
+	private int health = 100;
 	
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getIntellegence() {
+		return intellegence;
+	}
+
+	public void setIntellegence(int intellegence) {
+		this.intellegence = intellegence;
+	}
+
+	public int getStealth() {
+		return stealth;
+	}
+
+	public void setStealth(int stealth) {
+		this.stealth = stealth;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public void attack(Human human) {
+		human.health -= this.health;
+	}
+	
+	public int displayHealth() {
+		return getHealth();
+	}
 }
--- 01_JAVA_OOP/A_JAVA_OOP/14_ASSIGNMENT_05_OBJECT_MASTER_PART_ONE/src/com/codingdojo/assignment/objectmasterpartone/HumanTest.java
+++ 01_JAVA_OOP/A_JAVA_OOP/14_ASSIGNMENT_05_OBJECT_MASTER_PART_ONE/src/com/codingdojo/assignment/objectmasterpartone/HumanTest.java
@@ -4,7 +4,12 @@ public class HumanTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Human human1 = new Human();
+		Human human2 = new Human();
 		
+		human1.attack(human2);
+		System.out.println(human1.displayHealth());
+		System.out.println(human2.displayHealth());
 	}
 
 }
