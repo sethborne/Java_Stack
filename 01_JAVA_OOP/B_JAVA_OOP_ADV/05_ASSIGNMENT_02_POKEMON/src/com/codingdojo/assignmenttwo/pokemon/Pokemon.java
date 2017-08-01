472fd32fe1e37b6f7dc39e572e70a6be76abb61e
Seth Borne <sethborne@gmail.com>
2017-08-01 14:17:46 -0700

    Java OOP Adv - 05 - Assignment 02 - Pokemon - Third Commit

---
+++ 01_JAVA_OOP/B_JAVA_OOP_ADV/05_ASSIGNMENT_02_POKEMON/src/com/codingdojo/assignmenttwo/pokemon/Pokemon.java
@@ -0,0 +1,5 @@
+package com.codingdojo.assignmenttwo.pokemon;
+
+public class Pokemon {
+
+}
---
+++ 01_JAVA_OOP/B_JAVA_OOP_ADV/05_ASSIGNMENT_02_POKEMON/src/com/codingdojo/assignmenttwo/pokemon/PokemonTest.java
@@ -0,0 +1,10 @@
+package com.codingdojo.assignmenttwo.pokemon;
+
+public class PokemonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
