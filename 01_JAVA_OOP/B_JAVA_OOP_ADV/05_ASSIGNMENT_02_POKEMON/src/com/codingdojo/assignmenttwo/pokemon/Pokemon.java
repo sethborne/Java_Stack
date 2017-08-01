3a5c5b98c6345601b0adb5ecfb43713584e2bb9a
Seth Borne <sethborne@gmail.com>
2017-08-01 15:20:46 -0700

    Java OOP Adv - 05 - Assignment 02 - Pokemon - Third Commit

--- 01_JAVA_OOP/B_JAVA_OOP_ADV/05_ASSIGNMENT_02_POKEMON/src/com/codingdojo/assignmenttwo/pokemon/Pokemon.java
+++ 01_JAVA_OOP/B_JAVA_OOP_ADV/05_ASSIGNMENT_02_POKEMON/src/com/codingdojo/assignmenttwo/pokemon/Pokemon.java
@@ -1,5 +1,24 @@
 package com.codingdojo.assignmenttwo.pokemon;
 
 public class Pokemon {
-
+	private String name;
+	private int health;
+	private String type;
+	
+	private static int numberOfPokemon = 0;
+	
+	public Pokemon() {
+		numberOfPokemon += 1;
+	}
+	
+	public Pokemon(String name, int health, String type) {
+		this.name = name;
+		this.health = health;
+		this.type = type;
+		numberOfPokemon += 1;
+	}
+	
+	public static int pokemonCount() {
+		return numberOfPokemon;
+	}
 }
--- 01_JAVA_OOP/B_JAVA_OOP_ADV/05_ASSIGNMENT_02_POKEMON/src/com/codingdojo/assignmenttwo/pokemon/PokemonTest.java
+++ 01_JAVA_OOP/B_JAVA_OOP_ADV/05_ASSIGNMENT_02_POKEMON/src/com/codingdojo/assignmenttwo/pokemon/PokemonTest.java
@@ -4,7 +4,10 @@ public class PokemonTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Pokemon poke1 = new Pokemon("bulbasaur", 100, "Land");
+		Pokemon poke2 = new Pokemon("bulbasaur2", 150, "Land2");
+		
+		System.out.println(Pokemon.pokemonCount());
 	}
 
 }
